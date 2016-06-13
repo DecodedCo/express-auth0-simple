749653a747a99c53bdc3da06222f155dc7ab859a
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-06-13 17:43:49 +0100

    Fix typo in env variable name

--- auth.js
+++ auth.js
@@ -18,7 +18,7 @@ var OPTIONS = {
   auth0: {
     domain: 'decoded.eu.auth0.com',
     clientID: process.env.AUTH0_CLIENT_ID,
-    clientSecret: process.env.AUTH0,
+    clientSecret: process.env.AUTH0_CLIENT_SECRET,
     callbackURL: '/callback'
   },
   cookieSecret: uuid.v4(),
