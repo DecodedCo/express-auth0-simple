6f04e0c6d844d2cc90e4e65ed8057b17f053aa94
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-09-22 16:44:46 +0100

    Initial WIP on Object-oriented interface for module

--- auth.js
+++ auth.js
@@ -15,17 +15,67 @@ var uuid = require('node-uuid');
 // for overriding options object
 var merge = require('merge');
 
-// global module default config object
-var OPTIONS = {
-  auth0: {
-    callbackURL: '/auth/callback'
-  },
-  cookieSecret: uuid.v4(),
-  successRedirect: '/',
-  failureRedirect: '/',
-  serializeUser: null,
-  deserializeUser: null
-};
+/*
+ * Main prototype constructor
+ * Call this once with your express app instance as the first argument and an
+ * options object as the second. This will setup and initialise one instance of
+ * the middleware.
+ */
+module.exports = function(app, options) {
+  // define private variables
+  var _options = {
+    auth0: {
+      callbackURL: '/auth/callback'
+    },
+    cookieSecret: uuid.v4(),
+    successRedirect: '/',
+    failureRedirect: '/',
+    serializeUser: null,
+    deserializeUser: null
+  };
+
+  // reload options from environment variables
+  reloadConfig(false);
+
+  // override any options that were specified
+  if (options) {
+    _options = merge.recursive(_options, options);
+  }
+
+  // public methods
+  return {
+    /*
+     * This is called by module constructor and is used to load Auth0 config variables
+     * from environment variables. You can also call it yourself if you want.
+     * By default, this doesn't override variables that already exit, but you
+     * can optionally tell it to override all variables with the environment ones
+     * if you want, with the sole function argument.
+     */
+    reloadConfig: function(override) {
+      if (override) {
+        _options.auth0 = merge(
+          _options.auth0,
+          {
+            domain: process.env.AUTH0_DOMAIN,
+            clientID: process.env.AUTH0_CLIENT_ID,
+            clientSecret: process.env.AUTH0_CLIENT_SECRET
+          }
+        );
+      } else {
+        if (!_options.auth0.domain) {
+          _options.auth0.domain = process.env.AUTH0_DOMAIN;
+        }
+        if (!_options.auth0.clientID) {
+          _options.auth0.clientID = process.env.AUTH0_CLIENT_ID;
+        }
+        if (!_options.auth0.clientSecret) {
+          _options.auth0.clientSecret = process.env.AUTH0_CLIENT_SECRET;
+        }
+      }
+    },
+    ...
+  };
+}
 
 // passportjs verify callback for the Auth0Strategy
 var verifyCallback = function (
@@ -53,36 +103,6 @@ var authCallbackHandler = function (req, res) {
     throw new Error('user null');
   }
   res.redirect(OPTIONS.successRedirect);
-}
-
-/*
- * This is called by init() and is used to load Auth0 config variables from
- * environment variables. You can also call it yourself if you want.
- * By default, this doesn't override variables that already exit, but you
- * can optionally tell it to override all variables with the environment ones
- * if you want, with the sole function argument.
- */
-exports.reloadConfig = function(override) {
-  if (override) {
-    OPTIONS.auth0 = merge(
-      OPTIONS.auth0,
-      {
-        domain: process.env.AUTH0_DOMAIN,
-        clientID: process.env.AUTH0_CLIENT_ID,
-        clientSecret: process.env.AUTH0_CLIENT_SECRET
-      }
-    );
-  } else {
-    if (!OPTIONS.auth0.domain) {
-      OPTIONS.auth0.domain = process.env.AUTH0_DOMAIN;
-    }
-    if (!OPTIONS.auth0.clientID) {
-      OPTIONS.auth0.clientID = process.env.AUTH0_CLIENT_ID;
-    }
-    if (!OPTIONS.auth0.clientSecret) {
-      OPTIONS.auth0.clientSecret = process.env.AUTH0_CLIENT_SECRET;
-    }
-  }
 }
 
 /*
