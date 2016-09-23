00e05887184204cc20bf4ff32761b4e3594d2d00
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-09-23 17:54:24 +0100

    Fix broken module prototype Assign public methods to 'this' and set private ones as local variables

--- auth.js
+++ auth.js
@@ -22,7 +22,7 @@ var merge = require('merge');
  * the middleware.
  */
 module.exports = function(app, options) {
-  // define private variables
+  // begin private variables
   var _options = {
     auth0: {
       callbackURL: '/auth/callback'
@@ -34,15 +34,80 @@ module.exports = function(app, options) {
     deserializeUser: null
   };
   var strategy;
+  // end private variables
 
+  // begin private methods
+  // passportjs verify callback for the Auth0Strategy
+  var verifyCallback = function (
+    accessToken, refreshToken, extraParams, profile, done
+  ) {
+    // accessToken is the token to call Auth0 API (not needed in the most cases)
+    // extraParams.id_token has the JSON Web Token
+    // profile has all the information from the user
+    return done(null, profile);
+  };
+  // passportjs default user serialisation/deserialisation functions
+  // This is not a best practice, but we want to keep things simple for now
+  var serializeUser = function (user, done) {
+    done(null, user);
+  };
+  var deserializeUser = function (user, done) {
+    done(null, user);
+  };
+  // the route handler for the auth0 callback
+  var authCallbackHandler = function (req, res) {
+    if (!req.user) {
+      throw new Error('user null');
+    }
+    res.redirect(_options.successRedirect);
+  };
+  // end private methods
+
+  // begin public methods
+  /*
+   * This is called by module constructor and is used to load Auth0 config variables
+   * from environment variables. You can also call it yourself if you want.
+   * By default, this doesn't override variables that already exit, but you
+   * can optionally tell it to override all variables with the environment ones
+   * if you want, with the sole function argument.
+   */
+  this.reloadConfig = function (override) {
+    if (override) {
+      _options.auth0 = merge(
+        _options.auth0,
+        {
+          domain: process.env.AUTH0_DOMAIN,
+          clientID: process.env.AUTH0_CLIENT_ID,
+          clientSecret: process.env.AUTH0_CLIENT_SECRET
+        }
+      );
+    } else {
+      if (!_options.auth0.domain) {
+        _options.auth0.domain = process.env.AUTH0_DOMAIN;
+      }
+      if (!_options.auth0.clientID) {
+        _options.auth0.clientID = process.env.AUTH0_CLIENT_ID;
+      }
+      if (!_options.auth0.clientSecret) {
+        _options.auth0.clientSecret = process.env.AUTH0_CLIENT_SECRET;
+      }
+    }
+  };
+  this.requiresLogin = function (req, res, next) {
+    if (!req.isAuthenticated()) {
+      return res.redirect(_options.auth0.callbackURL);
+    }
+    next();
+  };
+  // end public methods
+
+  // begin constructor function proper
   // reload options from environment variables
-  reloadConfig(false);
-
+  this.reloadConfig(false);
   // override any options that were specified
   if (options) {
     _options = merge.recursive(_options, options);
   }
-
   // build auth0 passportjs strategy
   strategy = new Auth0Strategy(_options.auth0, verifyCallback);
   passport.use(strategy);
@@ -71,67 +136,5 @@ module.exports = function(app, options) {
     ),
     authCallbackHandler
   );
-
-  // public methods
-  return {
-    /*
-     * This is called by module constructor and is used to load Auth0 config variables
-     * from environment variables. You can also call it yourself if you want.
-     * By default, this doesn't override variables that already exit, but you
-     * can optionally tell it to override all variables with the environment ones
-     * if you want, with the sole function argument.
-     */
-    reloadConfig: function (override) {
-      if (override) {
-        _options.auth0 = merge(
-          _options.auth0,
-          {
-            domain: process.env.AUTH0_DOMAIN,
-            clientID: process.env.AUTH0_CLIENT_ID,
-            clientSecret: process.env.AUTH0_CLIENT_SECRET
-          }
-        );
-      } else {
-        if (!_options.auth0.domain) {
-          _options.auth0.domain = process.env.AUTH0_DOMAIN;
-        }
-        if (!_options.auth0.clientID) {
-          _options.auth0.clientID = process.env.AUTH0_CLIENT_ID;
-        }
-        if (!_options.auth0.clientSecret) {
-          _options.auth0.clientSecret = process.env.AUTH0_CLIENT_SECRET;
-        }
-      }
-    },
-    // passportjs verify callback for the Auth0Strategy
-    verifyCallback: function (
-      accessToken, refreshToken, extraParams, profile, done
-    ) {
-      // accessToken is the token to call Auth0 API (not needed in the most cases)
-      // extraParams.id_token has the JSON Web Token
-      // profile has all the information from the user
-      return done(null, profile);
-    },
-    // passportjs default user serialisation/deserialisation functions
-    // This is not a best practice, but we want to keep things simple for now
-    serializeUser: function (user, done) {
-      done(null, user);
-    },
-    deserializeUser: function (user, done) {
-      done(null, user);
-    },
-    // the route handler for the auth0 callback
-    authCallbackHandler: function (req, res) {
-      if (!req.user) {
-        throw new Error('user null');
-      }
-      res.redirect(OPTIONS.successRedirect);
-    },
-    requiresLogin: function (req, res, next) {
-      if (!req.isAuthenticated()) {
-        return res.redirect(OPTIONS.auth0.callbackURL);
-      }
-      next();
-    }
-  };
+  // end constructor function proper
 };
