a4b3d7d241a68f50480c1554930ae9b95ae67e63
Joshua Saxby <saxbophone@users.noreply.github.com>
2016-09-28 16:53:00 +0100

    Rename reloadConfig() to loadConfig() and turn into private method This is because reloading config state once initialised is not possible (or desirable)

--- auth.js
+++ auth.js
@@ -37,6 +37,20 @@ module.exports = function(app, options) {
   // end private variables
 
   // begin private methods
+  /*
+   * This is called by module constructor and is used to load Auth0 config variables
+   * from environment variables.
+   */
+  var loadConfig = function () {
+    _options.auth0 = merge(
+      _options.auth0,
+      {
+        domain: process.env.AUTH0_DOMAIN,
+        clientID: process.env.AUTH0_CLIENT_ID,
+        clientSecret: process.env.AUTH0_CLIENT_SECRET
+      }
+    );
+  };
   // passportjs verify callback for the Auth0Strategy
   var verifyCallback = function (
     accessToken, refreshToken, extraParams, profile, done
@@ -64,35 +78,6 @@ module.exports = function(app, options) {
   // end private methods
 
   // begin public methods
-  /*
-   * This is called by module constructor and is used to load Auth0 config variables
-   * from environment variables. You can also call it yourself if you want.
-   * By default, this doesn't override variables that already exit, but you
-   * can optionally tell it to override all variables with the environment ones
-   * if you want, with the sole function argument.
-   */
-  this.reloadConfig = function (override) {
-    if (override) {
-      _options.auth0 = merge(
-        _options.auth0,
-        {
-          domain: process.env.AUTH0_DOMAIN,
-          clientID: process.env.AUTH0_CLIENT_ID,
-          clientSecret: process.env.AUTH0_CLIENT_SECRET
-        }
-      );
-    } else {
-      if (!_options.auth0.domain) {
-        _options.auth0.domain = process.env.AUTH0_DOMAIN;
-      }
-      if (!_options.auth0.clientID) {
-        _options.auth0.clientID = process.env.AUTH0_CLIENT_ID;
-      }
-      if (!_options.auth0.clientSecret) {
-        _options.auth0.clientSecret = process.env.AUTH0_CLIENT_SECRET;
-      }
-    }
-  };
   // Use this middleware to protect one or more routes from access without auth
   this.requiresLogin = function (req, res, next) {
     if (!req.isAuthenticated()) {
@@ -103,8 +88,8 @@ module.exports = function(app, options) {
   // end public methods
 
   // begin constructor function proper
-  // reload options from environment variables
-  this.reloadConfig(false);
+  // load options from environment variables
+  loadConfig();
   // override any options that were specified
   if (options) {
     _options = merge.recursive(_options, options);
