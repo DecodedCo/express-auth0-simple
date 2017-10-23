758b83f72f2b5c645505059d300a240cc11688a0
Joshua Saxby <josh@decoded.com>
2017-10-23 13:15:12 +0100

    Re-arrance, clean up, comment and remove old deps

--- src/auth.js
+++ src/auth.js
@@ -14,18 +14,13 @@
 
 // Auth0 custom passportjs auth strategy
 import Auth0Strategy from 'passport-auth0';
-// cookie middleware for keeping user logged in
-import cookieParser from 'cookie-parser';
 // for overriding options object
 import merge from 'merge';
 // passportjs
 import passport from 'passport';
-// Session middleware for keeping user logged in
-import session from 'express-session';
-// used for generating uuid for cookie secret
-import uuid from 'uuid';
 
 
+// this is a bit of a hack to allow private attributes in ES6 classes
 let _private = {};
 for (const item of ['auth0Options', 'options', 'requiresLogin', 'strategy']) {
   _private[item] = Symbol(item);
@@ -87,6 +82,7 @@ export class ExpressAuth0Middleware {
     done(null, user);
   };
 
+  // used for the login/callback route
   get authCallbackMiddleware () {
     return passport.authenticate(
       'auth0',
@@ -94,6 +90,7 @@ export class ExpressAuth0Middleware {
     );
   };
 
+  // this is the login/callback route
   authCallbackHandler (req, res) {
     if (!req.user) {
       throw new Error('user null');
@@ -105,6 +102,19 @@ export class ExpressAuth0Middleware {
     }
   };
 
+  // returns the middeware method for ensuring requests are logged in
+  get requiresLogin () {
+    return this[_private.requiresLogin].bind(this);
+  };
+
+  /*
+   * this is the middleware for ensuring a request is logged in
+   * it's private because it can't be used directly, as express clears the
+   * value of the `this` pointer when mounting it as middleware.
+   * To get around this, one has to refer to it with function.prototype.bind()
+   * to ensure the value of `this` refers to this class instance, so we wrap
+   * this functionality in a getter method so the user doesn't notice.
+   */
   [_private.requiresLogin] (req, res, next) {
     if (!req.isAuthenticated()) {
       // store requested URL for redirecting back to when authenticated
@@ -114,130 +124,4 @@ export class ExpressAuth0Middleware {
       next();
     }
   };
-
-  get requiresLogin () {
-    return this[_private.requiresLogin].bind(this);
-  };
 };
-
-/*
- * Main prototype constructor
- * Call this once with your express app instance as the first argument and an
- * options object as the second. This will setup and initialise one instance of
- * the middleware.
- */
-// module.exports = function(app, options) {
-//   // begin private variables
-//   var _options = {
-//     auth0: {
-//       callbackURL: '/auth/callback'
-//     },
-//     cookieSecret: uuid.v4(),
-//     successRedirect: '/',
-//     failureRedirect: '/auth/failed',
-//     serializeUser: null,  // optional pointer to a callback function
-//     deserializeUser: null, // as above
-//     // if set to true, the library will automatically provide a failure route
-//     useDefaultFailureRoute: true
-//   };
-//   var strategy;
-//   // end private variables
-
-//   // begin private methods
-//   /*
-//    * This is called by module constructor and is used to load Auth0 config
-//    * variables from environment variables.
-//    */
-//   var loadConfig = function () {
-//     _options.auth0 = merge(
-//       _options.auth0,
-//       {
-//         domain: process.env.AUTH0_DOMAIN,
-//         clientID: process.env.AUTH0_CLIENT_ID,
-//         clientSecret: process.env.AUTH0_CLIENT_SECRET
-//       }
-//     );
-//   };
-//   // passportjs verify callback for the Auth0Strategy
-//   var verifyCallback = function (
-//     accessToken, refreshToken, extraParams, profile, done
-//   ) {
-//     // accessToken is the token to call Auth0 API (not needed in the most
-//     // cases)
-//     // extraParams.id_token has the JSON Web Token
-//     // profile has all the information from the user
-//     return done(null, profile);
-//   };
-//   // passportjs default user serialisation/deserialisation functions
-//   // This is not a best practice, but we want to keep things simple for now
-//   var serializeUser = function (user, done) {
-//     done(null, user);
-//   };
-//   var deserializeUser = function (user, done) {
-//     done(null, user);
-//   };
-//   // the route handler for the auth0 callback
-//   var authCallbackHandler = function (req, res) {
-//     if (!req.user) {
-//       throw new Error('user null');
-//     }
-//     res.redirect(_options.successRedirect);
-//   };
-//   // end private methods
-
-//   // begin public methods
-//   // Use this middleware to protect one or more routes from access without auth
-//   this.requiresLogin = function (req, res, next) {
-//     if (!req.isAuthenticated()) {
-//       return res.redirect(_options.auth0.callbackURL);
-//     }
-//     next();
-//   };
-//   // end public methods
-
-//   // begin constructor function proper
-//   // load options from environment variables
-//   loadConfig();
-//   // override any options that were specified
-//   if (options) {
-//     _options = merge.recursive(_options, options);
-//   }
-//   // build auth0 passportjs strategy
-//   strategy = new Auth0Strategy(_options.auth0, verifyCallback);
-//   passport.use(strategy);
-//   // register passportjs serialisation/deserialisation functions
-//   passport.serializeUser(_options.serializeUser || serializeUser);
-//   passport.deserializeUser(_options.deserializeUser || deserializeUser);
-//   // register cookie and session middlewares
-//   app.use(cookieParser());
-//   app.use(
-//     session(
-//       {
-//         secret: _options.cookieSecret,
-//         resave: false,
-//         saveUninitialized: false
-//       }
-//     )
-//   );
-//   // register passportjs middlewares
-//   app.use(passport.initialize());
-//   app.use(passport.session());
-//   // create Auth0 callback handler
-//   app.get(
-//     _options.auth0.callbackURL,
-//     passport.authenticate(
-//       'auth0', { failureRedirect: _options.failureRedirect }
-//     ),
-//     authCallbackHandler
-//   );
-//   // create passportjs/Auth0 failure route if requested
-//   if (_options.useDefaultFailureRoute) {
-//     app.get(
-//       _options.failureRedirect,
-//       function (req, res) {
-//         res.sendStatus(403);
-//       }
-//     )
-//   }
-//   // end constructor function proper
-// };
